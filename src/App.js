32b7a84042a9f2e9fff078e629a620cd256f79ab
wuyaheng <wuyaheng2016@gmail.com>
2020-06-23 14:47:53 -0400

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Layout/Navbar'
+import Users from './components/users/Users'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,9 @@ class App extends Component {
     return (
       <div className="App">
       <Navbar />
+      <div className="container">
+      <Users />
+      </div>
       </div>
     );
   }
---
+++ src/components/users/Users.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import UserItem from './UserItem'
+
+export class Users extends Component {
+    state = {
+        users: [
+            {
+                id: '1',
+                login: 'mojombo',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url: 'http://github.com/mojombo'
+            },
+            {
+                id: '2',
+                login: 'defunkt',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+                html_url: 'http://github.com/defunkt'
+            },
+            {
+                id: '3',
+                login: 'mojombo',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url: 'http://github.com/mojombo'
+            },
+            {
+                id: '4',
+                login: 'mojombo',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url: 'http://github.com/mojombo'
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div style={userStyle}>
+                {this.state.users.map(user => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        )
+    }
+}
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+
+export default Users
